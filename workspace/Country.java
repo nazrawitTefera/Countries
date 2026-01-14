3ffbe5d8edf3a83ded6a75d4a312a5cc7d1ae914
naz <nazikefyalew@gmail.com>
2026-01-14 01:23:33 +0000

    country done

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,14 +1,40 @@
+ 
+ //nazrawit tefera
+ //date- jan 23, 2026
+ //
 public class Country
 {
   // add private instance variables for the name, capital, language, and image file.
-
+   private String country;
+   private String language;
+   private String image;
+   private String capital;
   // add constructors
+  public guess(String country, String language, String image, String capital ){
+    this.country = country;
+    this.language = language;
+    this.image= image;
+    this.capital= capital;
+  }
 
   // Write accessor/get methods for each instance variable that returns it.
-
+public String getCountry(){
+  return country;
+}
+public String getLanguage(){
+  return language;
+}
+public String getImage(){
+  return image;
+}
+public String getCapital(){
+  return capital;
+}
   // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
  
-
+public String toString(){
+  return country + "'s capital is " + capital + " and its primary language is " + language + ".";
+}
 
   
 }
